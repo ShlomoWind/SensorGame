4f08bc2d976a2b160e392eb85d7184d5a6639b42
Shlomo <sh.z.wind@gmail.com>
2025-06-17 16:36:08 +0300

    created sensor factory and one class sensor ligth

---
+++ SensorFile/SensorFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorGame.SensorFile
+{
+    internal class SensorFactory
+    {
+        public static Sensor CreateSensor(string type) 
+        {
+            switch(type.ToLower())
+            {
+                case "light":
+                    return new SensorLight();
+                default:
+                    return null;
+            }
+        }
+    }
+}
